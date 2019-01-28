cc39459236c368138073c3b2818d8bcbd09450eb
Joel Plowright <joelplow@gmail.com>
2019-01-28 14:33:35 +0000

    room numbers

--- test/test_descriptions.js
+++ test/test_descriptions.js
@@ -6,6 +6,6 @@ for(var i=0; i<10; i++){
   var house = new TownHouse()
   console.log(house.getDescriptiveReference())
   for(var j in house.allRooms)
-   console.log("\t", house.allRooms[j].getDescriptiveReference())
+   console.log("\t", parseInt(j)+1+".", house.allRooms[j].getDescriptiveReference())
   console.log("\n")
 }
