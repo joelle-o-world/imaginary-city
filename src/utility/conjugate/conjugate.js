b346d5a6cbd108d99ef92542d41ff3e97ca1ad8f
Joel Plowright <joelplow@gmail.com>
2019-02-12 16:46:15 +0000

    conjugate.js - anyPersonRegex, form constants

--- src/utility/conjugate/conjugate.js
+++ src/utility/conjugate/conjugate.js
@@ -17,13 +17,27 @@ VERB FORMS DENOTED AS NUMBERS:
   (9. past tense form)
 */
 
+const regOp = require("../regex")
 const irregular = require("./irregularConjugations")
 
 const endsWithShortConsonant = /[aeiou][tpdn]$/
 const endsWithE = /e$/
 const endsWithO = /o$/
 
+const FIRST_PERSON_SINGULAR = 1   // I
+const SECOND_PERSON_SINGULAR = 2  // you
+const THIRD_PERSON_SINGULAR = 3   // he/she/it
+const FIRST_PERSON_PLURAL = 4     // we
+const SECOND_PERSON_PLURAL = 5    // you
+const THIRD_PERSON_PLURAL = 6     // they
+const GERUND = 7
+const PAST_PARTICIPLE = 8
+const PAST_TENSE = 9
+const ALL_PERSON_REGEX = 10
+
 function conjugate(infinitive, form) {
+  if(form == ALL_PERSON_REGEX)
+    return anyPersonRegex(infinitive)
   if(irregular[infinitive] && irregular[infinitive][form])
     return irregular[infinitive][form]
   return conjugateRegular(infinitive, form)
@@ -32,14 +46,14 @@ function conjugate(infinitive, form) {
 function conjugateRegular(infinitive, form) {
   switch(form) {
     // third person singular
-    case 3:
+    case THIRD_PERSON_SINGULAR:
       if(endsWithO.test(infinitive))
         return infinitive+'es'
       else
         return infinitive+'s'
 
     // gerund
-    case 7:
+    case GERUND:
       if(endsWithE.test(infinitive))
         return infinitive.slice(0, infinitive.length-1)+'ing'
       if(endsWithShortConsonant.test(infinitive))
@@ -47,8 +61,8 @@ function conjugateRegular(infinitive, form) {
       return infinitive+'ing'
 
     // past participle
-    case 9:
-    case 8:
+    case PAST_TENSE:
+    case PAST_PARTICIPLE:
       if(endsWithShortConsonant.test(infinitive))
         return infinitive + infinitive[infinitive.length-1]+'ed'
       if(endsWithE.test(infinitive))
@@ -61,5 +75,16 @@ function conjugateRegular(infinitive, form) {
   }
 }
 
+function anyPersonRegex(infinitive) {
+  let forms = []
+  for(let person=1; person<=6; ++person) {
+    let form = conjugate(infinitive, person)
+    if(!forms.includes(form))
+      forms.push(form)
+  }
+  return regOp.or(...forms)
+}
+
 
 module.exports = conjugate
+conjugate.anyPersonRegex
