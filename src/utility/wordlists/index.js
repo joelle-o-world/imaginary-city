21e739207ae28c3a91d6251099daa0790cc04361
Joel Plowright <joelplow@gmail.com>
2019-02-14 15:47:54 +0000

    add wordlists

---
+++ src/utility/wordlists/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  nouns: require("./nouns.words.json"),
+  adjectives: require('./adjectives.words.json'),
+  verbs: require('./verbs.words.json'),
+  adverbs: require('./adverbs.words.json/)
+}
